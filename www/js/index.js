83b24de8d5b0e3d0da6c9214e4e7173caecdb97c
Arkadiusz Klenczar <a.klenczar@gmail.com>
2014-09-19 16:12:51 +0200

    href

--- www/js/index.js
+++ www/js/index.js
@@ -18,7 +18,7 @@ $(document).on('pageinit', '#login', function(){
                         },
                         success: function (result) {
                             if(result.status) {
-                                $.mobile.changePage("#second");                        
+                                $.mobile.changePage("index_bkp.html");                        
                             } else {
                                 alert('Logon unsuccessful!');
                             }
